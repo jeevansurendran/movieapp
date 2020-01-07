4dc6c7ca3a7c9bcc1e08e8f2b2e6c6c3eb2f6a17
Jeevan Surendran <jvns67@gmail.com>
2020-01-07 12:44:24 +0530

    Fragments working except the back button

--- app/src/main/java/com/silverpants/movieapp/activity/DiscoverActivity.kt
+++ app/src/main/java/com/silverpants/movieapp/activity/DiscoverActivity.kt
@@ -1,25 +1,94 @@
 package com.silverpants.movieapp.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.ViewModelProviders
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.silverpants.movieapp.R
+import com.silverpants.movieapp.fragment.DiscoverFragment
+import com.silverpants.movieapp.fragment.SavedFragment
+import com.silverpants.movieapp.fragment.SearchFragment
+import com.silverpants.movieapp.fragment.UpdateFragment
+import com.silverpants.movieapp.viewmodel.DiscoverViewModel
 
 class DiscoverActivity : AppCompatActivity() {
 
 
     private lateinit var bottomNavigationView: BottomNavigationView
     private lateinit var fragmentManager: FragmentManager
+    private lateinit var discoverViewModel: DiscoverViewModel
+    private val discoverFragment = DiscoverFragment()
+    private val searchFragment = SearchFragment()
+    private val savedFragment = SavedFragment()
+    private val updateFragment = UpdateFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_discover)
 
+        //setup
         bottomNavigationView = findViewById(R.id.movie_bottom_navigation)
         fragmentManager = supportFragmentManager
+        discoverViewModel = ViewModelProviders.of(this)[DiscoverViewModel::class.java]
+        val fragmentIdMap = mapOf(R.id.movie_item_discover to discoverFragment, R.id.movie_item_search to searchFragment,
+                R.id.movie_item_saved to savedFragment, R.id.movie_item_update to updateFragment)
 
-        bottomNavigationView.setOnNavigationItemReselectedListener {
+        //add fragment to the container
+        fragmentManager.beginTransaction()
+                .add(R.id.movie_fragment_container, discoverFragment).hide(discoverFragment)
+                .add(R.id.movie_fragment_container, searchFragment).hide(searchFragment)
+                .add(R.id.movie_fragment_container, savedFragment).hide(savedFragment)
+                .add(R.id.movie_fragment_container, updateFragment).hide(updateFragment).commit()
+
+
+        //retrieve the fragment ID from the view model and update the active fragment accordingly
+        var activeFragment = fragmentIdMap[discoverViewModel.activeFragmentId] ?: discoverFragment
+        fragmentManager.beginTransaction().show(activeFragment).commit()
+        bottomNavigationView.selectedItemId = discoverViewModel.activeFragmentId
+
+        Log.d(DiscoverActivity::class.simpleName, "b\n${discoverViewModel.activeFragmentId}\n $activeFragment\n $discoverFragment \n $searchFragment \n $savedFragment \n $updateFragment ${fragmentManager.backStackEntryCount}")
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            if (it.itemId != discoverViewModel.activeFragmentId) {
+                when (it.itemId) {
+                    R.id.movie_item_discover -> {
+                        fragmentManager.beginTransaction().show(discoverFragment).hide(activeFragment).commit()
+                        activeFragment = discoverFragment
+                        discoverViewModel.activeFragmentId = R.id.movie_item_discover
+
+                    }
+                    R.id.movie_item_search -> {
+                        fragmentManager.beginTransaction().show(searchFragment).hide(activeFragment).commit()
+                        activeFragment = searchFragment
+                        discoverViewModel.activeFragmentId = R.id.movie_item_search
+
+                    }
+                    R.id.movie_item_saved -> {
+                        fragmentManager.beginTransaction().show(savedFragment).hide(activeFragment).commit()
+                        activeFragment = savedFragment
+                        discoverViewModel.activeFragmentId = R.id.movie_item_saved
+
+                    }
+                    R.id.movie_item_update -> {
+                        fragmentManager.beginTransaction().show(updateFragment).hide(activeFragment).commit()
+                        activeFragment = updateFragment
+                        discoverViewModel.activeFragmentId = R.id.movie_item_update
+
+                    }
+                }
+                Log.d(DiscoverActivity::class.simpleName, "b\n${discoverViewModel.activeFragmentId
+                }\n $activeFragment\n $discoverFragment\n $searchFragment\n $savedFragment\n $updateFragment ${fragmentManager.backStackEntryCount}")
+                return@setOnNavigationItemSelectedListener true
+            }
+            return@setOnNavigationItemSelectedListener false
+
         }
     }
 
+    override fun onStop() {
+        fragmentManager.beginTransaction().remove(discoverFragment).remove(searchFragment).remove(savedFragment).remove(updateFragment).commit()
+        super.onStop()
+    }
 }
--- app/src/main/java/com/silverpants/movieapp/viewmodel/DiscoverViewModel.kt
+++ app/src/main/java/com/silverpants/movieapp/viewmodel/DiscoverViewModel.kt
@@ -1,17 +1,20 @@
 package com.silverpants.movieapp.viewmodel
 
-import com.silverpants.movieapp.pojo.discover.Result
-import com.silverpants.movieapp.retrofit.RetroFitRepository
-
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.paging.PagedList
+import com.silverpants.movieapp.R
+import com.silverpants.movieapp.pojo.discover.Result
+import com.silverpants.movieapp.retrofit.RetroFitRepository
 
 
 class DiscoverViewModel : ViewModel() {
 
-    private var retroFitRepository: RetroFitRepository = RetroFitRepository
-    var dataInvalidated: Boolean = false
+    private var retroFitRepository = RetroFitRepository
+    var dataInvalidated = false
+    var activeFragmentId: Int = R.id.movie_item_discover
+
 
     private var discoverList: LiveData<PagedList<Result>> = retroFitRepository.discoverList
 
@@ -23,6 +26,7 @@ class DiscoverViewModel : ViewModel() {
         return discoverList
 
     }
+
 
     fun getDiscover(year: Int, sort_by: String): LiveData<PagedList<Result>> {
         if (dataInvalidated) {
